f1e8773ea54b0b2788a69e03b1d8c53a4f10374c
Massimo Nocentini <massimo.nocentini@gmail.com>
2026-03-19 12:38:11 +0100

    Refactor parse_censo function to accept legend filename and return legend data
    - Modified parse_censo to take an additional parameter for the legend CSV file.
    - Implemented reading of the legend file to create a dictionary mapping field names to definitions.
    - Updated the function return to include both the parsed rows and the legend.
    - Adjusted the function call to use the new parameters and updated file paths.

--- src/censoparser.py
+++ src/censoparser.py
@@ -2,7 +2,7 @@
 import csv
 
 
-def parse_censo(file_path):
+def parse_censo(file_path, legend_filename):
 
     rows = []
     
@@ -14,11 +14,30 @@ def parse_censo(file_path):
             
             rows.append(row)
 
-    return rows
+    legend = {}
+
+    with open(legend_filename, "r") as csvfile:
+
+        spamreader = csv.DictReader(csvfile, delimiter=',')
+
+        for row in spamreader:
+            
+            legend[row['NOME_CAMPO']] = row['DEFINIZIONE']
+
+    return rows, legend
 
 
-rows = parse_censo('../data/censo/censo-2021.csv')
+rows, legend = parse_censo('../data/censo/censo-2021-grouped.csv', '../data/censo/censo-legenda.csv')
 
 print(rows[0])
+# print(legend)
 
-    
+# import pandas as pd
+
+# df = pd.read_csv('../data/censo/censo-2021.csv', delimiter=',', thousands=".", decimal=",")
+
+# df_grouped = df.groupby("COMUNE").sum(numeric_only=True)
+
+# df_grouped
+
+# df_grouped.to_csv('../data/censo/censo-2021-grouped.csv', sep=',', decimal=',', index=True)
